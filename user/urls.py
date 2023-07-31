eb4f18ec753e00124295a85a540e458bc1dafe3a
Dmytro Shvets <dirolius@gmail.com>
2023-07-31 18:43:51 +0300

    Add JWT to project. Add register & manager views

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,18 @@
-urlpatterns = []
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+from user.views import RegisterView, ManageUserView
+
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="create"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+]
 
 app_name = "user"
