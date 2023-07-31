5e08c10d471afe207191f9170b65f1be76f8c9ab
Dmytro Shvets <dirolius@gmail.com>
2023-07-31 19:52:53 +0300

    Add JWT.blacklist for logout functionality

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
+    TokenBlacklistView,
 )
 
 from user.views import RegisterView, ManageUserView
@@ -12,7 +13,8 @@ urlpatterns = [
     path("me/", ManageUserView.as_view(), name="manage"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("logout/", TokenBlacklistView.as_view(), name="logout"),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -2,7 +2,6 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from user.models import User
 from user.serializers import UserSerializer
 
 
